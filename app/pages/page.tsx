406b9fcfe5fc34b25a7a19f8eff05814ae6a1c96
Bridger Tower <bridgertower@gmail.com>
2023-10-05 14:12:01 -0600

    create author, tag, posts and pages route

---
+++ app/pages/page.tsx
@@ -0,0 +1,77 @@
+import settings from "@/config/settings.json";
+import Link from "next/link";
+import Image from "next/image";
+
+type Page = {
+  id: number;
+  title: {
+    rendered: string;
+  };
+  date: string;
+  slug: string;
+  excerpt: {
+    rendered: string;
+  };
+  _embedded: {
+    "wp:featuredmedia": Array<{
+      media_details: {
+        sizes: {
+          medium: {
+            source_url: string;
+          };
+        };
+      };
+    }>;
+  };
+};
+
+export default async function Pages() {
+  async function getPages() {
+    const res = await fetch(
+      `https://${settings.url}/wp-json/wp/v2/pages?_embed&per_page=${settings.limit}`
+    );
+
+    if (!res.ok) {
+      throw new Error("Failed to fetch data");
+    }
+
+    return res.json();
+  }
+
+  let data;
+  try {
+    data = await getPages();
+  } catch (error) {
+    console.error(error);
+  }
+
+  if (!data) {
+    return <div>Loading...</div>;
+  }
+
+  return (
+    <main className="p-12">
+      <h1 className="mb-12">Pages</h1>
+
+      <div className="grid grid-cols-3 gap-6">
+        {data.map((page: Page) => (
+          <Link
+            href={`pages/${page.slug}`}
+            className="p-6 border hover:bg-slate-200 flex flex-col gap-4"
+            key={page.id}
+          >
+            <h3 className="mb-2 text-lg font-semibold">
+              title: {page.title.rendered}
+            </h3>
+            <p>date: {new Date(page.date).toLocaleDateString()}</p>
+            <div
+              dangerouslySetInnerHTML={{
+                __html: page.excerpt.rendered.split(".")[0],
+              }}
+            />
+          </Link>
+        ))}
+      </div>
+    </main>
+  );
+}
---
+++ app/posts/author/page.tsx
@@ -0,0 +1,94 @@
+import settings from "@/config/settings.json";
+import Link from "next/link";
+import Image from "next/image";
+
+type Author = {
+  id: number;
+  name: string;
+  posts: Array<Post>;
+};
+
+type Post = {
+  id: number;
+  title: {
+    rendered: string;
+  };
+  date: string;
+  slug: string;
+  excerpt: {
+    rendered: string;
+  };
+  _embedded: {
+    "wp:featuredmedia": Array<{
+      media_details: {
+        sizes: {
+          medium: {
+            source_url: string;
+          };
+        };
+      };
+    }>;
+  };
+};
+
+export default async function AuthorPosts() {
+  async function getAuthorPosts() {
+    const res = await fetch(
+      `https://${settings.url}/wp-json/wp/v2/users?_embed&per_page=${settings.limit}`
+    );
+
+    if (!res.ok) {
+      throw new Error("Failed to fetch data");
+    }
+
+    return res.json();
+  }
+
+  let data;
+  try {
+    data = await getAuthorPosts();
+  } catch (error) {
+    console.error(error);
+  }
+
+  if (!data) {
+    return <div>Loading...</div>;
+  }
+
+  return (
+    <main className="p-12">
+      <h1 className="mb-12">Author Posts</h1>
+
+      <div className="grid grid-cols-3 gap-6">
+        {data.map((author: Author) =>
+          author.posts.map((post: Post) => (
+            <Link
+              href={`posts/${post.slug}`}
+              className="p-6 border hover:bg-slate-200 flex flex-col gap-4"
+              key={post.id}
+            >
+              <img
+                src={
+                  post._embedded["wp:featuredmedia"][0].media_details.sizes
+                    .medium.source_url
+                }
+                width={1080}
+                height={400}
+                alt="Post image"
+              />
+              <h3 className="mb-2 text-lg font-semibold">
+                title: {post.title.rendered}
+              </h3>
+              <p>date: {new Date(post.date).toLocaleDateString()}</p>
+              <div
+                dangerouslySetInnerHTML={{
+                  __html: post.excerpt.rendered.split(".")[0],
+                }}
+              />
+            </Link>
+          ))
+        )}
+      </div>
+    </main>
+  );
+}
