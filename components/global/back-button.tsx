8e88dcea4ab9c6b4120bcd408318144207a77ce5
Bridger Tower <bridgertower@gmail.com>
2023-10-10 12:46:51 -0600

    ui: style back button

--- components/global/back-button.tsx
+++ components/global/back-button.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { useRouter } from 'next/navigation'
+import { ArrowLeft } from 'lucide-react';
 
 const BackButton = () => {
   const router = useRouter()
@@ -10,7 +11,10 @@ const BackButton = () => {
   }
 
   return (
-    <button className='' onClick={handleBackButtonClick}>Go Back</button>
+    <button className='text-sm flex gap-1 items-center my-6' onClick={handleBackButtonClick}>
+      <ArrowLeft className='w-4' />
+      <span className='border-b'>Go Back</span>
+    </button>
   )
 }
 
