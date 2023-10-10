88c13fb658465536ce332304c7cef4c821e788cc
Bridger Tower <bridgertower@gmail.com>
2023-10-10 16:01:48 -0600

    ui: updates to post card component

--- app/(pages)/all/page.tsx
+++ app/(pages)/all/page.tsx
@@ -53,21 +53,23 @@ export default async function Pages() {
     <main className="grid gap-6">
       <h1 className="text-4xl">All Pages for {settings.site_name}</h1>
       <h2 className="text-2xl">These are all the pages from WordPress for the website. When you create a new page in WordPress you will see it appear here.</h2>
-      <div className="grid grid-cols-3 gap-6">
+      <div className="grid gap-6">
         {data.map((page: Page) => (
           <Link
-            href={`pages/${page.slug}`}
-            className="p-6 border hover:bg-slate-200 flex flex-col gap-4"
+            href={`/${page.slug}`}
+            className="hover:bg-slate-50 transition-all rounded-r-md flex flex-col gap-2 p-4 border-l-2"
             key={page.id}
           >
-            <h3 className="mb-2 text-lg font-semibold" dangerouslySetInnerHTML={{
-              __html: page.title.rendered,
-            }}>
-            </h3>
-            <p>date: {new Date(page.date).toLocaleDateString()}</p>
+            <div className="flex items-baseline gap-2">
+              <h3 className="mb-2 text-lg underline underline-offset-4 font-semibold" dangerouslySetInnerHTML={{
+                __html: page.title.rendered,
+              }}>
+              </h3>
+              <p>date: {new Date(page.date).toLocaleDateString()}</p>
+            </div>
             <div
               dangerouslySetInnerHTML={{
-                __html: page.excerpt.rendered.split(".")[0],
+                __html: page.excerpt.rendered,
               }}
             />
           </Link>
--- components/content/post-card.tsx
+++ components/content/post-card.tsx
@@ -1,6 +1,6 @@
 import Link from "next/link";
 import React from "react";
-import { Calendar } from "lucide-react";
+import { Calendar, ArrowUpRight } from "lucide-react";
 
 type Post = {
   id: number;
@@ -50,12 +50,16 @@ const PostCard: React.FC<PostCardProps> = ({ post, tags }) => {
             <img
               src={post._embedded["wp:featuredmedia"]?.[0]?.media_details?.sizes?.full?.source_url}
               alt="Post image"
-              className="absolute top-0 left-0 w-full h-full object-cover rounded-lg"
+              className="absolute top-0 left-0 w-full h-full object-cover rounded-lg border"
             />
+            <div className="hidden transition-all group-hover:flex text-sm items-center absolute bottom-2 right-2 rounded-md bg-slate-200 bg-opacity-75 backdrop-blur-md">
+              <p className="sr-only">Read More</p> <ArrowUpRight className="" />
+            </div>
           </div>
 
-        )}
-      <p className="text-xs flex items-center gap-1"><Calendar className="w-4" />{date.toDateString()}</p>
+        )
+      }
+      <p className="text-xs text-slate-500 flex items-center gap-1"><Calendar className="w-4" />{date.toDateString()}</p>
       <h3
         dangerouslySetInnerHTML={{ __html: post.title.rendered }}
         className="mb-2 text-slate-600 text-lg font-semibold group-hover:underline group-hover:underline-offset-4"
@@ -68,26 +72,28 @@ const PostCard: React.FC<PostCardProps> = ({ post, tags }) => {
         }}
       />
 
-      {post.tags && post.tags.length > 0 && (
-        <div>
-          <ul>
-            {post.tags.map((tagId, index) => {
-              const tag = tags.find((t) => t.id === tagId);
-              return (
-                <li key={index}>
-                  <Link
-                    className="px-2 py-1 bg-slate-200 w-fit rounded-md text-sm"
-                    href="/"
-                  >
-                    {tag?.name}
-                  </Link>
-                </li>
-              );
-            })}
-          </ul>
-        </div>
-      )}
-    </Link>
+      {
+        post.tags && post.tags.length > 0 && (
+          <div>
+            <ul>
+              {post.tags.map((tagId, index) => {
+                const tag = tags.find((t) => t.id === tagId);
+                return (
+                  <li key={index}>
+                    <Link
+                      className="px-2 py-1 bg-slate-200 w-fit rounded-md text-sm"
+                      href="/"
+                    >
+                      {tag?.name}
+                    </Link>
+                  </li>
+                );
+              })}
+            </ul>
+          </div>
+        )
+      }
+    </Link >
   );
 };
 
