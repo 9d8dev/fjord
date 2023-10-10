0edd78affa6e3c4e0e941d5c297bd19810012903
Bridger Tower <bridgertower@gmail.com>
2023-10-10 10:17:18 -0600

    fix: updates to nav

--- components/global/nav.tsx
+++ components/global/nav.tsx
@@ -1,7 +1,10 @@
+"use client";
+
 import Link from "next/link";
 import Image from "next/image";
 import Logo from "@/public/logo.svg";
 import settings from "@/fjord.json";
+import { usePathname } from "next/navigation";
 
 const Nav = () => {
   const links = [
@@ -10,24 +13,38 @@ const Nav = () => {
     { href: "/posts", text: "Blog" },
     { href: "/contact", text: "Contact" },
   ];
+  const pathname = usePathname();
 
   return (
     <nav className="sticky top-0 py-4 text-sm">
       <div className="container rounded-xl bg-opacity-75 border-2 backdrop-blur-md flex justify-between items-center m-auto p-4 bg-slate-200">
-        <Link className="flex gap-2 items-center hover:opacity-75" href="/">
+        <Link
+          className="flex gap-2 items-center hover:opacity-75 transition-all"
+          href="/"
+        >
           <Image className="w-16" src={Logo} alt="logo"></Image>
           <p className="sr-only">{settings.site_name}</p>
         </Link>
         <ul className="flex gap-4">
           {links.map((link, index) => (
             <li key={index}>
-              <Link className="" href={link.href}>
+              <Link
+                className={`hover:underline underline-offset-4 transition-all ${
+                  pathname === link.href
+                    ? "opacity-50 hover:no-underline cursor-default"
+                    : ""
+                }`}
+                href={link.href}
+              >
                 {link.text}
               </Link>
             </li>
           ))}
         </ul>
-        <Link className="px-3 py-2 rounded-lg bg-slate-600 text-white" href="/">
+        <Link
+          className="px-3 py-2 rounded-lg bg-slate-600 text-white hover:mb-px hover:-mt-px transition-all"
+          href="/"
+        >
           Get Started
         </Link>
       </div>
