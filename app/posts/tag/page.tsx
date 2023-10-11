9d9e789d63fd3281824ea42b80e8682f8667274e
Bridger Tower <bridgertower@gmail.com>
2023-10-11 09:18:40 -0600

    feat: tag page working

--- app/posts/tag/page.tsx
+++ app/posts/tag/page.tsx
@@ -1,29 +1,5 @@
 import settings from "@/fjord.json";
 import Link from "next/link";
-import Image from "next/image";
-
-type Post = {
-  id: number;
-  title: {
-    rendered: string;
-  };
-  date: string;
-  slug: string;
-  excerpt: {
-    rendered: string;
-  };
-  _embedded: {
-    "wp:featuredmedia": Array<{
-      media_details: {
-        sizes: {
-          medium: {
-            source_url: string;
-          };
-        };
-      };
-    }>;
-  };
-};
 
 type Tag = {
   id: number;
@@ -31,10 +7,10 @@ type Tag = {
   slug: string;
 };
 
-export default async function Posts({ tagId }: { tagId: number }) {
-  async function getPostsByTag() {
+export default async function TagPosts() {
+  async function getTagPosts() {
     const res = await fetch(
-      `https://${settings.url}/wp-json/wp/v2/posts?_embed&tags=${tagId}&per_page=${settings.limit}`
+      `https://${settings.url}/wp-json/wp/v2/tags?_embed&per_page=${settings.limit}`
     );
 
     if (!res.ok) {
@@ -44,37 +20,31 @@ export default async function Posts({ tagId }: { tagId: number }) {
     return res.json();
   }
 
-  async function getAllTags() {
-    const res = await fetch(`https://${settings.url}/wp-json/wp/v2/tags`);
-
-    if (!res.ok) {
-      throw new Error("Failed to fetch data");
-    }
-
-    return res.json();
-  }
-
   let data;
-  let tags;
   try {
-    data = await getPostsByTag();
-    tags = await getAllTags();
+    data = await getTagPosts();
   } catch (error) {
     console.error(error);
+    return <div>Error: Failed to fetch data</div>;
   }
 
-  if (!data || !tags) {
+  if (!data) {
     return <div>Loading...</div>;
+  }
+
+  if (!Array.isArray(data)) {
+    console.error("Invalid data format");
+    return <div>Error: Invalid data format</div>;
   }
 
   return (
     <main className="p-12">
-      <h1 className="mb-12">Tags</h1>
+      <h1 className="mb-12">Tag Posts</h1>
       <ul>
-        {tags.map((tag: Tag) => (
+        {data.map((tag: Tag) => (
           <li key={tag.id}>
-            <Link href={`/posts/tag/${tag.slug}`}>
-              <a>{tag.name}</a>
+            <Link href={`/tag/${tag.slug}`}>
+              <p>{tag.name}</p>
             </Link>
           </li>
         ))}
