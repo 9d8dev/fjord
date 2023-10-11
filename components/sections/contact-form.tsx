282ba6a3ad9bec446ea123c3492e0366e250b763
Bridger Tower <bridgertower@gmail.com>
2023-10-11 11:47:06 -0600

    fix: apos

--- components/sections/contact-form.tsx
+++ components/sections/contact-form.tsx
@@ -193,7 +193,7 @@ export default function Example() {
             type="submit"
             className="block w-full rounded-md bg-indigo-600 px-3.5 py-2.5 text-center text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
           >
-            Let's talk
+            Let&apos;s talk
           </button>
         </div>
       </form>
