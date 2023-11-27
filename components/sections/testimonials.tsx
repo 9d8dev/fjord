e63727ed381938eb44cc9c007a7ad964a0c1be7f
Bridger Tower <bridgertower@gmail.com>
2023-11-27 16:33:59 -0700

    fix

--- components/sections/testimonials.tsx
+++ components/sections/testimonials.tsx
@@ -34,7 +34,7 @@ const Testimonials = () => {
     <Section>
       <Container>
         <div className="flex flex-col gap-6">
-          <h3 className="text-4xl">Don't just take our word for it.</h3>
+          <h3 className="text-4xl">Don&apos;t just take our word for it.</h3>
           <h4 className="text-2xl font-thin opacity-70">
             Find out why so many trust {fjord.site_name} for their headless
             WordPress setup.
@@ -43,7 +43,7 @@ const Testimonials = () => {
           <div className="grid md:grid-cols-2 mt-6 gap-6 md:mt-12">
             {testimonialsText.map(({ logo, name, quote, title }, index) => (
               <div className="grid gap-4" key={index}>
-                <h5 className="text-lg font-thin">"{quote}"</h5>
+                <h5 className="text-lg font-thin">&quot;{quote}&quot;</h5>
                 <div>
                   <h4 className="text-xl text-primary-500">{name}</h4>
                   <p className="font-light opacity-70">{title}</p>
