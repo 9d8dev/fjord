a4fd9702748bdd2076a0eb6b7da44a37081e6f7c
Bridger Tower <bridgertower@gmail.com>
2023-12-07 11:28:33 -0700

    validation to 1 hour

--- fjord.config.ts
+++ fjord.config.ts
@@ -8,7 +8,7 @@ const fjord: fjordTypes = {
   posts_per_page: 6,
   wordpress_url: "https://windpress.wpenginepowered.com",
   google_analytics_id: "G-7VM8KZENP7",
-  revalidate: 60,
+  revalidate: 3600, // revalidate time in seconds
 
   links: {
     main_cta: "#",
