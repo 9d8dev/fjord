f5df3debc49ac1feb72a9040f8f7a759fbee5eba
Bridger Tower <bridgertower@gmail.com>
2024-01-09 09:35:58 -0700

    initialize shadcn/ui

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,78 +1,80 @@
-import colors from "tailwindcss/colors";
-import type { Config } from "tailwindcss";
-import fjord from "./fjord.config";
+import type { Config } from "tailwindcss"
 
-function getColorScheme(colorName: string) {
-  switch (colorName) {
-    case "slate":
-      return colors.slate;
-    case "gray":
-      return colors.gray;
-    case "zinc":
-      return colors.zinc;
-    case "neutral":
-      return colors.neutral;
-    case "stone":
-      return colors.stone;
-    case "red":
-      return colors.red;
-    case "orange":
-      return colors.orange;
-    case "amber":
-      return colors.amber;
-    case "yellow":
-      return colors.yellow;
-    case "lime":
-      return colors.lime;
-    case "green":
-      return colors.green;
-    case "emerald":
-      return colors.emerald;
-    case "teal":
-      return colors.teal;
-    case "cyan":
-      return colors.cyan;
-    case "sky":
-      return colors.sky;
-    case "blue":
-      return colors.blue;
-    case "indigo":
-      return colors.indigo;
-    case "violet":
-      return colors.violet;
-    case "purple":
-      return colors.purple;
-    case "fuchsia":
-      return colors.fuchsia;
-    case "pink":
-      return colors.pink;
-    case "rose":
-      return colors.rose;
-    default:
-      return colors.slate;
-  }
-}
-
-const config: Config = {
-  // toggle to 'class' to disable dark mode, see: https://tailwindcss.com/docs/dark-mode#toggling-dark-mode-manually
-  darkMode: "media",
+const config = {
+  darkMode: ["class"],
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
+    './pages/**/*.{ts,tsx}',
+    './components/**/*.{ts,tsx}',
+    './app/**/*.{ts,tsx}',
+    './src/**/*.{ts,tsx}',
+	],
+  prefix: "",
   theme: {
+    container: {
+      center: true,
+      padding: "2rem",
+      screens: {
+        "2xl": "1400px",
+      },
+    },
     extend: {
       colors: {
-        primary: getColorScheme(fjord.theme.primary),
-        secondary: getColorScheme(fjord.theme.secondary),
+        border: "hsl(var(--border))",
+        input: "hsl(var(--input))",
+        ring: "hsl(var(--ring))",
+        background: "hsl(var(--background))",
+        foreground: "hsl(var(--foreground))",
+        primary: {
+          DEFAULT: "hsl(var(--primary))",
+          foreground: "hsl(var(--primary-foreground))",
+        },
+        secondary: {
+          DEFAULT: "hsl(var(--secondary))",
+          foreground: "hsl(var(--secondary-foreground))",
+        },
+        destructive: {
+          DEFAULT: "hsl(var(--destructive))",
+          foreground: "hsl(var(--destructive-foreground))",
+        },
+        muted: {
+          DEFAULT: "hsl(var(--muted))",
+          foreground: "hsl(var(--muted-foreground))",
+        },
+        accent: {
+          DEFAULT: "hsl(var(--accent))",
+          foreground: "hsl(var(--accent-foreground))",
+        },
+        popover: {
+          DEFAULT: "hsl(var(--popover))",
+          foreground: "hsl(var(--popover-foreground))",
+        },
+        card: {
+          DEFAULT: "hsl(var(--card))",
+          foreground: "hsl(var(--card-foreground))",
+        },
+      },
+      borderRadius: {
+        lg: "var(--radius)",
+        md: "calc(var(--radius) - 2px)",
+        sm: "calc(var(--radius) - 4px)",
+      },
+      keyframes: {
+        "accordion-down": {
+          from: { height: "0" },
+          to: { height: "var(--radix-accordion-content-height)" },
+        },
+        "accordion-up": {
+          from: { height: "var(--radix-accordion-content-height)" },
+          to: { height: "0" },
+        },
+      },
+      animation: {
+        "accordion-down": "accordion-down 0.2s ease-out",
+        "accordion-up": "accordion-up 0.2s ease-out",
       },
     },
   },
-  plugins: [require("@tailwindcss/typography")],
-  corePlugins: {
-    transform: true,
-  },
-};
+  plugins: [require("tailwindcss-animate")],
+} satisfies Config
 
-export default config;
+export default config
