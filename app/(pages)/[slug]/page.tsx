26be6250e696878b1ea84972b2b8f9711b09468b
Bridger Tower <bridgertower@gmail.com>
2024-01-11 13:00:31 -0700

    fix: types

--- app/(pages)/[slug]/page.tsx
+++ app/(pages)/[slug]/page.tsx
@@ -1,35 +1,61 @@
-import PageBody from "@/components/content/page-body";
+// Fjord Config
 import fjord from "@/fjord.config";
+
+// Component Imports
+import Article from "@/components/content/article-wrapper";
+import RecentPosts from "@/components/sections/recent-posts";
+import CTA from "@/components/sections/cta";
+
+// Next Imports
 import { notFound } from "next/navigation";
 import type { Metadata } from "next";
-import { fetchPage } from "@/lib/data";
+
+// Data Imports
+import { fetchPostBySlug } from "@/lib/data";
 
 export async function generateStaticParams() {
-  const res = await fetch(`${fjord.wordpress_url}/wp-json/wp/v2/pages?_embed`, {
+  const res = await fetch(`${fjord.wordpress_url}/wp-json/wp/v2/posts?_embed`, {
     next: { revalidate: 3600 },
   });
 
-  const data: PageProps[] = await res.json();
+  const data: PostProps[] = await res.json();
 
-  return data.map((page) => ({
-    slug: page?.slug,
+  return data.map((post) => ({
+    slug: post?.slug,
   }));
 }
 
 export default async function Page({ params }: { params: { slug: string } }) {
-  const page: PageProps = await fetchPage(params?.slug);
-  if (!page) {
-    return { notFound: true };
+  const post: PostProps = await fetchPostBySlug(params?.slug);
+  if (!post) {
+    return notFound();
   }
-  const date = new Date(page.date);
+
+  const date = new Date(post.date);
+  const author = post._embedded?.author?.[0] ?? null;
+
   const metadata: Metadata = {
-    title: `${page.title.rendered} | ${fjord.site_name}`,
-    description: page.excerpt.rendered,
+    title: `${post.title.rendered} | ${fjord.site_name}`,
+    description: post.excerpt?.rendered,
   };
 
+  // Ensure that the excerpt and author are not undefined before passing to the Article component
+  if (!post.excerpt || !author) {
+    return notFound();
+  }
+
   return (
-    <div className="p-6">
-      <PageBody {...page} date={date.toISOString()} />
+    <div>
+      <Article
+        post={{
+          ...post,
+          excerpt: post.excerpt || { rendered: "" },
+        }}
+        date={date}
+        author={author}
+      />
+      <RecentPosts />
+      <CTA />
     </div>
   );
 }
