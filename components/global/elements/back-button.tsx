5c4fdb60d3cf2688be9ceee805e50e9a25514b00
Bridger Tower <bridgertower@gmail.com>
2024-01-11 13:15:55 -0700

    fix: types?

--- components/global/elements/back-button.tsx
+++ components/global/elements/back-button.tsx
@@ -1,27 +1,27 @@
-"use client";
+'use client';
 
-import { useRouter } from "next/navigation";
-import { ArrowLeftIcon } from "@radix-ui/react-icons";
-import { Button } from "@/components/ui/button";
-import { cn } from "@/lib/utils";
+import { useRouter } from 'next/navigation';
+import { ArrowLeftIcon } from '@radix-ui/react-icons';
+import { Button } from '@/components/ui/button';
+import { cn } from '@/lib/utils';
 
 const BackButton = (className?: any) => {
-  const router = useRouter();
+	const router = useRouter();
 
-  const handleBackButtonClick = () => {
-    router.back();
-  };
+	const handleBackButtonClick = () => {
+		router.back();
+	};
 
-  return (
-    <Button
-      variant="outline"
-      className={cn("not-prose text-sm", className)}
-      onClick={handleBackButtonClick}
-    >
-      <ArrowLeftIcon className="w-4 mr-2" />
-      <span>Go Back</span>
-    </Button>
-  );
+	return (
+		<Button
+			variant="outline"
+			className={cn('not-prose text-sm', className)}
+			onClick={handleBackButtonClick}
+		>
+			<ArrowLeftIcon className="w-4 mr-2" />
+			<span>Go Back</span>
+		</Button>
+	);
 };
 
 export default BackButton;
--- components/sections/about.tsx
+++ components/sections/about.tsx
@@ -1,25 +1,23 @@
-import * as Craft from "@/components/craft/layout";
+import * as Craft from '@/components/craft/layout';
 
 const About = () => {
-  return (
-    <Craft.Section>
-      <Craft.Container>
-        <div className="flex flex-col gap-6">
-          <h3 className="text-4xl">Fjord is a headless WordPress template</h3>
-          <h4 className="text-2xl font-thin opacity-70">
-            This is an example about section. What do you want to say about your
-            company?
-          </h4>
-          <p className="font-light opacity-70 md:w-3/5 mt-6 md:mt-12">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-            aliquip ex ea commodo consequat.
-          </p>
-        </div>
-      </Craft.Container>
-    </Craft.Section>
-  );
+	return (
+		<Craft.Section>
+			<Craft.Container>
+				<div className="flex flex-col gap-6">
+					<h3 className="text-4xl">Fjord is a headless WordPress template</h3>
+					<h4 className="text-2xl font-thin opacity-70">
+						This is an example about section. What do you want to say about your company?
+					</h4>
+					<p className="font-light opacity-70 md:w-3/5 mt-6 md:mt-12">
+						Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+						incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
+						exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+					</p>
+				</div>
+			</Craft.Container>
+		</Craft.Section>
+	);
 };
 
 export default About;
