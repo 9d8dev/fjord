a5e3bc9521583e644c8427cc3d989a1c881de7fe
Bridger Tower <bridgertower@gmail.com>
2024-01-11 13:33:46 -0700

    updates to secondary-header

--- components/sections/secondary-hero.tsx
+++ components/sections/secondary-hero.tsx
@@ -1,26 +1,26 @@
-import * as Craft from "@/components/craft/layout";
-import { Badge } from "@/components/ui/badge";
+import * as Craft from '@/components/craft/layout';
+import { Badge } from '@/components/ui/badge';
 
 export default function SecondaryHero({
-  title,
-  subtitle,
-  children,
-  className,
+	title,
+	subtitle,
+	children,
+	className
 }: {
-  title: string;
-  subtitle?: string;
-  children?: React.ReactNode;
-  className?: string;
+	title: string;
+	subtitle?: string;
+	children?: React.ReactNode;
+	className?: string;
 }) {
-  return (
-    <Craft.Section className="border-b">
-      <Craft.Container>
-        <div className="mx-auto max-w-4xl lg:mx-0 not-prose">
-          <Badge variant="outline">{subtitle}</Badge>
-          <h2>{title}</h2>
-          <p>{children}</p>
-        </div>
-      </Craft.Container>
-    </Craft.Section>
-  );
+	return (
+		<Craft.Section className="border-b">
+			<Craft.Container>
+				<div className="mx-auto max-w-4xl not-prose">
+					<Badge variant="outline">{subtitle}</Badge>
+					<h2 className="text-4xl mt-4 mb-6">{title}</h2>
+					<p>{children}</p>
+				</div>
+			</Craft.Container>
+		</Craft.Section>
+	);
 }
