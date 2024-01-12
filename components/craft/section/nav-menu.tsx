f718da97bf0750346c843b8108ea17b2e3329955
Bridger Tower <bridgertower@gmail.com>
2024-01-12 10:09:06 -0700

    ui: nav menu

--- components/craft/section/nav-menu.tsx
+++ components/craft/section/nav-menu.tsx
@@ -55,7 +55,7 @@ export function NavMenu() {
         <NavigationMenuItem>
           <NavigationMenuTrigger>{fjord.nav_menu.title}</NavigationMenuTrigger>
           <NavigationMenuContent>
-            <ul className="grid w-[400px] gap-3 p-4 md:w-[500px] md:grid-cols-2 lg:w-[600px] ">
+            <ul className="grid w-[400px] gap-3 p-4 ">
               {fjord.nav_menu.items.map((component) => (
                 <ListItem
                   key={component.title}
